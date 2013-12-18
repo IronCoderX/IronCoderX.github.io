f75b43ea9040e82fae749e17b97e7dd06c36154f
Michael Reynolds <reynmaker.enterprises@gmail.com>
2013-12-18 00:19:17 -0600

    email contact form update

--- javascripts/script.js
+++ javascripts/script.js
@@ -32,6 +32,50 @@ $(function() {
         $('#honors').show();
     });
     
+    $('.error').hide();
+    $(".button").click(function() {
+      // validate and process form here
+      
+      $('.error').hide();
+  	  var name = $("input#name").val();
+  		if (name == "") {
+        $("label#name_error").show();
+        $("input#name").focus();
+        return false;
+      }
+  		var email = $("input#email").val();
+  		if (email == "") {
+        $("label#email_error").show();
+        $("input#email").focus();
+        return false;
+      }
+  		var phone = $("input#phone").val();
+  		if (phone == "") {
+        $("label#phone_error").show();
+        $("input#phone").focus();
+        return false;
+      }
+        
+        var dataString = 'name='+ name + '&email=' + email + '&phone=' + phone;
+  //alert (dataString);return false;
+  $.ajax({
+    type: "POST",
+    url: "bin/process.php",
+    data: dataString,
+    success: function() {
+      $('#contact_form').html("<div id='message'></div>");
+      $('#message').html("<h2>Contact Form Submitted!</h2>")
+      .append("<p>We will be in touch soon.</p>")
+      .hide()
+      .fadeIn(1500, function() {
+        $('#message').append("<img id='checkmark' src='images/check.png' />");
+      });
+    }
+  });
+  return false;
+      
+    });
+    
 });
 
 var portfolioImages = new Array();
@@ -101,3 +145,5 @@ function replaceExample(placement)
     
     return false;
 }
+
+
